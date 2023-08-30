7d249f2b8174c023dcb0b1170bfe4aeb23d67817
Stanislav Ashurkov <lalkastrel@gmail.com>
2023-08-30 14:07:57 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Fune = new System.Windows.Forms.PictureBox();
+            this.LeftMove = new System.Windows.Forms.Timer(this.components);
+            this.RightMove = new System.Windows.Forms.Timer(this.components);
+            this.moveBulletsTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveEnemiesTimer = new System.Windows.Forms.Timer(this.components);
+            this.gameOver = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RestartText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Fune)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Fune
+            // 
+            this.Fune.BackgroundImage = global::Game.Properties.Resources.fune;
+            this.Fune.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Fune.Location = new System.Drawing.Point(455, 619);
+            this.Fune.Name = "Fune";
+            this.Fune.Size = new System.Drawing.Size(62, 82);
+            this.Fune.TabIndex = 0;
+            this.Fune.TabStop = false;
+            this.Fune.Click += new System.EventHandler(this.Fune_Click);
+            // 
+            // LeftMove
+            // 
+            this.LeftMove.Enabled = true;
+            this.LeftMove.Tick += new System.EventHandler(this.LeftMove_Tick);
+            // 
+            // RightMove
+            // 
+            this.RightMove.Enabled = true;
+            this.RightMove.Tick += new System.EventHandler(this.RightMove_Tick);
+            // 
+            // moveBulletsTimer
+            // 
+            this.moveBulletsTimer.Enabled = true;
+            this.moveBulletsTimer.Interval = 1;
+            this.moveBulletsTimer.Tick += new System.EventHandler(this.moveBulletsTimer_Tick);
+            // 
+            // MoveEnemiesTimer
+            // 
+            this.MoveEnemiesTimer.Enabled = true;
+            this.MoveEnemiesTimer.Interval = 5;
+            this.MoveEnemiesTimer.Tick += new System.EventHandler(this.MoveEnemiesTimer_Tick);
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.BackColor = System.Drawing.Color.Transparent;
+            this.gameOver.Font = new System.Drawing.Font("Rubik", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gameOver.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gameOver.Location = new System.Drawing.Point(221, 205);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(525, 114);
+            this.gameOver.TabIndex = 1;
+            this.gameOver.Text = "Game over";
+            this.gameOver.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rubik", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Rubik", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(100, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // RestartText
+            // 
+            this.RestartText.AutoSize = true;
+            this.RestartText.BackColor = System.Drawing.Color.Transparent;
+            this.RestartText.Font = new System.Drawing.Font("Rubik", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RestartText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RestartText.Location = new System.Drawing.Point(271, 319);
+            this.RestartText.Name = "RestartText";
+            this.RestartText.Size = new System.Drawing.Size(401, 42);
+            this.RestartText.TabIndex = 4;
+            this.RestartText.Text = "Press R to restart game";
+            this.RestartText.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.RestartText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gameOver);
+            this.Controls.Add(this.Fune);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Fune)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Fune;
+        private System.Windows.Forms.Timer LeftMove;
+        private System.Windows.Forms.Timer RightMove;
+        private System.Windows.Forms.Timer moveBulletsTimer;
+        private System.Windows.Forms.Timer MoveEnemiesTimer;
+        private System.Windows.Forms.Label gameOver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label RestartText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        int funeSpeed;
+        int HP;
+        const int maxHP = 10;
+
+        PictureBox[] hpBar;
+
+        PictureBox[] bullets;
+        int bulletsSpeed;
+
+        PictureBox[] enemies;
+        int sizeEnemy;
+        int enemiesSpeed;
+
+        int score;
+
+        WindowsMediaPlayer Shoot;
+        WindowsMediaPlayer music;
+        WindowsMediaPlayer kill;
+        WindowsMediaPlayer death;
+        WindowsMediaPlayer damage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Fune_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LeftMove_Tick(object sender, EventArgs e)
+        {
+            if (Fune.Left > 50)
+            {
+                Fune.Left -= funeSpeed;
+            }
+        }
+
+        private void RightMove_Tick(object sender, EventArgs e)
+        {
+            if (Fune.Right < 950)
+            {
+                Fune.Left += funeSpeed;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            funeSpeed = 50;
+            HP = maxHP;
+
+            hpBar = new PictureBox[HP];
+            for (int i = 0; i < HP; i++)
+            {
+                int size = 1000 / HP;
+                hpBar[i] = new PictureBox();
+                hpBar[i].Visible = true;
+                hpBar[i].Size = new Size(size, 20);
+                hpBar[i].Location = new Point(i * size, 730);
+                hpBar[i].BackColor = Color.White;
+
+                this.Controls.Add(hpBar[i]);
+            }
+
+            bullets = new PictureBox[1];
+            bulletsSpeed = 60;
+
+            enemies = new PictureBox[4];
+            sizeEnemy = 50;
+            enemiesSpeed = 3;
+
+            score = 0;
+
+            Random rnd = new Random();
+            Image enemyImage = Image.FromFile("Assets\\Enemy.png");
+
+            Shoot = new WindowsMediaPlayer();
+            Shoot.URL = "Voice\\shoot.wav";
+            music = new WindowsMediaPlayer();
+            music.URL = "Voice\\music.mp3";
+            kill = new WindowsMediaPlayer();
+            kill.URL = "Voice\\kill.wav";
+            death = new WindowsMediaPlayer();
+            death.URL = "Voice\\death.wav";
+            damage = new WindowsMediaPlayer();
+            damage.URL = "Voice\\damage.wav";
+
+            Shoot.controls.stop();
+            kill.controls.stop();
+            death.controls.stop();
+            damage.controls.stop();
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i] = new PictureBox();
+                enemies[i].BackColor = Color.Black;
+                enemies[i].Image = enemyImage;
+                enemies[i].SizeMode = PictureBoxSizeMode.Zoom;
+                enemies[i].Size = new Size(sizeEnemy, sizeEnemy);
+                enemies[i].Location = new Point(rnd.Next(100, 900), rnd.Next(-1000, 0));
+
+                this.Controls.Add(enemies[i]);
+            }
+
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i] = new PictureBox();
+                bullets[i].BorderStyle = BorderStyle.None;
+                bullets[i].Size = new Size(5, 20);
+                bullets[i].BackColor = Color.White;
+
+                this.Controls.Add(bullets[i]);
+            }
+
+            music.controls.play();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                LeftMove.Start();
+            }
+
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                RightMove.Start();
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                Shoot.controls.stop();
+                Shoot.controls.play();
+
+                for (int i = 0; i < bullets.Length; i++)
+                {
+                    Intersect();
+
+                    if (bullets[i].Top < 1000)
+                    {
+                        bullets[i].Location = new Point(Fune.Location.X + 27, Fune.Location.Y + 30 + i * 50);
+                    }
+
+                }
+            }
+
+            if (e.KeyCode == Keys.R)
+            {
+                Restart();
+            }
+        }
+        
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                LeftMove.Stop();
+            }
+
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                RightMove.Stop();
+            }
+        }
+
+        private void moveBulletsTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i].Top -= bulletsSpeed;
+            }
+        }
+
+        private void MoveEnemiesTimer_Tick(object sender, EventArgs e)
+        {
+            MoveEnemies(enemies, enemiesSpeed);
+        }
+
+        private void MoveEnemies(PictureBox[] enemies, int speed)
+        {
+            Random rnd = new Random();
+
+            Intersect();
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].Top += speed;
+                if (enemies[i].Top > 750)
+                {
+                    enemies[i].Location = new Point(rnd.Next(100, 900), rnd.Next(-1000, 0));
+                    HP -= 1;
+                    damage.controls.stop();
+                    damage.controls.play();
+                    HPBar(HP);
+                    if (HP <= 0)
+                    {
+                        EndGame();
+                    }
+                }
+            }
+        }
+
+        private void Intersect()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                if (bullets[0].Bounds.IntersectsWith(enemies[i].Bounds))
+                {
+                    score++;
+                    label2.Text = score.ToString();
+                    kill.controls.stop();
+                    kill.controls.play();
+
+                    enemies[i].Location = new Point(rnd.Next(100, 900), rnd.Next(-500, 0));
+                    bullets[0].Location = new Point(3000, 1000);
+                }
+
+                if (Fune.Bounds.IntersectsWith(enemies[i].Bounds))
+                {
+                    HP -= 3;
+                    damage.controls.stop();
+                    damage.controls.play();
+                    HPBar(HP);
+                    enemies[i].Location = new Point(rnd.Next(100, 900), rnd.Next(-1000, 0));
+                    if (HP <= 0)
+                    {
+                        EndGame();
+                    }
+                }
+            }
+        }
+
+        private void EndGame()
+        {
+            gameOver.Visible = true;
+            RestartText.Visible = true;
+            Fune.Visible = false;
+            HPBar(-1);
+
+            MoveEnemiesTimer.Stop();
+            moveBulletsTimer.Stop();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void HPBar(int HP)
+        {
+            for (int i = 0; i < HP; i++)
+            {
+                hpBar[i].Visible = true;
+            }
+
+            if (HP < 0)
+            {
+                death.controls.play();
+                for (int i = 0; i < maxHP; i++)
+                {
+                    hpBar[i].Visible = false;
+                }
+                return;
+            }
+
+            for (int i = HP; i < maxHP; i++)
+            {
+                hpBar[i].Visible = false;
+            }
+        }
+
+        private void Restart()
+        {
+            gameOver.Visible = false;
+            RestartText.Visible = false;
+
+            HP = maxHP;
+            HPBar(HP);
+
+            Fune.Visible = true;
+            MoveEnemiesTimer.Start();
+            moveBulletsTimer.Start();
+            score = 0;
+            label2.Text = score.ToString();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].Location = new Point(rnd.Next(100, 900), rnd.Next(-1000, 0));
+            }
+
+            music.controls.stop();
+            music.controls.play();
+        }
+    }
+}
